8a34d5571d34cc82632378a9b993ed4bbb1989af
Extravi <98912029+Extravi@users.noreply.github.com>
2024-05-28 19:04:37 -0400

    Add files via upload

---
+++ start/src/main.cpp
@@ -0,0 +1,121 @@
+// Description: Bloxshade installer source code.
+// Author: Dante (dante@extravi.dev)
+// Date: 2024-05-08
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <Windows.h>
+#include <shellapi.h>
+#include <filesystem>
+#include <shlobj_core.h>
+
+// resource file
+#include "../resource.h"
+
+// namespace for fs
+namespace fs = std::filesystem;
+
+// bloxshade path
+const std::string bloxshadePath = "C:\\Program Files\\Bloxshade";
+const std::string installerPath = "C:\\Program Files\\Bloxshade\\setup.exe";
+
+bool ExtractEmbeddedExe(HRSRC hResInfo, HGLOBAL hResData, DWORD resourceSize,
+    const char* outputFilename) {
+    if (!hResInfo || !hResData || !resourceSize) {
+        // invalid resource
+        return false;
+    }
+
+    const BYTE* pData = (const BYTE*)LockResource(hResData);
+    if (!pData) {
+        // could not lock resource data
+        return false;
+    }
+
+    std::ofstream outputFile(outputFilename, std::ios::binary);
+    if (!outputFile.is_open()) {
+        // could not open output file
+        return false;
+    }
+
+    outputFile.write(reinterpret_cast<const char*>(pData), resourceSize);
+    outputFile.close();
+
+    FreeResource(hResData);
+    return true;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+    if (!IsUserAnAdmin()) {
+        MessageBox(NULL, L"Oops! You need to run this program as admin! >.< It's okay! If you don't have perms, ask your parent or guardian on how to run it as so!", L"Error", MB_ICONERROR | MB_OK);
+        return 0;
+    }
+
+    // kill any running process that may have the folder open
+    std::wstring killCommandInstaller = L"cmd.exe /c taskkill /F /IM installer.exe";
+    std::wstring killCommandSetup = L"cmd.exe /c taskkill /F /IM setup.exe";
+
+    // create process parameters
+    auto createAndCloseProcess = [](const std::wstring& command) {
+        STARTUPINFOW si = { sizeof(STARTUPINFO) };
+        PROCESS_INFORMATION pi;
+        if (CreateProcessW(NULL, const_cast<LPWSTR>(command.c_str()), NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
+            WaitForSingleObject(pi.hProcess, INFINITE);
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+        }
+        };
+
+    // wait for process to finish
+    createAndCloseProcess(killCommandInstaller);
+    createAndCloseProcess(killCommandSetup);
+
+    if (fs::exists(bloxshadePath)) {
+        std::cout << "Bloxshade folder true" << std::endl;
+        fs::remove_all(bloxshadePath);
+        fs::create_directories(bloxshadePath);
+    }
+    else {
+        std::cout << "Bloxshade folder false" << std::endl;
+        fs::create_directories(bloxshadePath);
+    }
+
+    HRSRC hResInfo;
+    HGLOBAL hResData;
+    DWORD resourceSize;
+
+    // extract setup.exe
+    hResInfo = FindResource(NULL, MAKEINTRESOURCE(SETUP_BINARY), L"BINARY");
+    if (hResInfo) {
+        hResData = LoadResource(NULL, hResInfo);
+        resourceSize = SizeofResource(NULL, hResInfo);
+        if (ExtractEmbeddedExe(hResInfo, hResData, resourceSize, (bloxshadePath + "\\setup.exe").c_str())) {
+            std::cout << "setup.exe extracted successfully." << std::endl;
+        }
+    }
+    else {
+        std::cerr << "Warning: setup.exe resource not found." << std::endl;
+    }
+
+    // extract installer.exe
+    hResInfo = FindResource(NULL, MAKEINTRESOURCE(INSTALLER_BINARY), L"BINARY");
+    if (hResInfo) {
+        hResData = LoadResource(NULL, hResInfo);
+        resourceSize = SizeofResource(NULL, hResInfo);
+        if (ExtractEmbeddedExe(hResInfo, hResData, resourceSize, (bloxshadePath + "\\installer.exe").c_str())) {
+            std::cout << "installer.exe extracted successfully." << std::endl;
+        }
+    }
+    else {
+        std::cerr << "Warning: installer.exe resource not found." << std::endl;
+    }
+
+    // start installer
+    WinExec(installerPath.c_str(), SW_HIDE);
+
+    // debug wait for input
+    //std::cin.get();
+    return 0;
+}
